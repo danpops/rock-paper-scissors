969f262c6b715c79c78441418c2aabeb07c56b7d
Dan Popovic <dpopovic@tribalscale.com>
2022-03-05 11:56:31 -0500

    fix: some refactoring

--- src/components/SelectMove/index.tsx
+++ src/components/SelectMove/index.tsx
@@ -1,33 +1,23 @@
 import React from 'react'
 import { Pressable } from 'react-native'
 import useDesign from '../../hooks/useDesign'
-import { useAppDispatch } from '../../store/hooks'
-import { playNewGame } from '../../store/slices/game.reducer'
-import getPlayerMoveIcon from '../../utils/imageDictionary'
+import useSelectMove from '../../hooks/useSelectMove'
 import { Heading3 } from '../Text'
 import { SelectMoveContainer, SelectMoveIcon, SelectMoveRow } from './styles'
 
 const SelectMove = () => {
-  const dispatch = useAppDispatch()
   const { fontColor } = useDesign()
-
-  const onSelectRock = () => dispatch(playNewGame('R'))
-  const onSelectPaper = () => dispatch(playNewGame('P'))
-  const onSelectScissors = () => dispatch(playNewGame('S'))
+  const { moveOptions } = useSelectMove()
 
   return (
     <SelectMoveContainer>
       <Heading3 color={fontColor}>Choose your move</Heading3>
       <SelectMoveRow>
-        <Pressable onPress={onSelectRock}>
-          <SelectMoveIcon source={getPlayerMoveIcon('R')} />
-        </Pressable>
-        <Pressable onPress={onSelectPaper}>
-          <SelectMoveIcon source={getPlayerMoveIcon('P')} />
-        </Pressable>
-        <Pressable onPress={onSelectScissors}>
-          <SelectMoveIcon source={getPlayerMoveIcon('S')} />
-        </Pressable>
+        {moveOptions.map(({ onPress, source }, index) => (
+          <Pressable key={index} onPress={onPress}>
+            <SelectMoveIcon source={source} />
+          </Pressable>
+        ))}
       </SelectMoveRow>
     </SelectMoveContainer>
   )
---
+++ src/hooks/useSelectMove.ts
@@ -0,0 +1,21 @@
+import { useAppDispatch } from '../store/hooks'
+import { playNewGame } from '../store/slices/game.reducer'
+import getPlayerMoveIcon from '../utils/imageDictionary'
+
+const useSelectMove = () => {
+  const dispatch = useAppDispatch()
+
+  const onSelectRock = () => dispatch(playNewGame('R'))
+  const onSelectPaper = () => dispatch(playNewGame('P'))
+  const onSelectScissors = () => dispatch(playNewGame('S'))
+
+  const moveOptions = [
+    { onPress: onSelectRock, source: getPlayerMoveIcon('R') },
+    { onPress: onSelectPaper, source: getPlayerMoveIcon('P') },
+    { onPress: onSelectScissors, source: getPlayerMoveIcon('S') },
+  ]
+
+  return { moveOptions }
+}
+
+export default useSelectMove
