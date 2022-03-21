d1e3a64a6cfcfc4cf3970e6965b085405b138862
Dan Popovic <dpopovic@tribalscale.com>
2022-03-21 15:51:32 -0400

    prevent user from selecting 2+ hands at once

--- src/components/Buttons/AnimatedHand/index.tsx
+++ src/components/Buttons/AnimatedHand/index.tsx
@@ -2,6 +2,8 @@ import React from 'react'
 import { ImageProps, StyleSheet } from 'react-native'
 import { PanGestureHandler } from 'react-native-gesture-handler'
 import Animated from 'react-native-reanimated'
+import { GameMoves } from '../../../lib/constants'
+import { useAppSelector } from '../../../store/hooks'
 import useHandAnimation from './animations/useHandAnimation'
 
 interface AnimatedHandInterface extends ImageProps {
@@ -12,11 +14,14 @@ interface AnimatedHandInterface extends ImageProps {
 const SIZE = 130.0
 
 const AnimatedHand = (props: AnimatedHandInterface) => {
-  const { onSwipe, ...imageProps } = props
+  const { onSwipe, move, ...imageProps } = props
+  const { userPlay } = useAppSelector((state) => state.game)
   const { panGestureEvent, shadowStyle, rStyle } = useHandAnimation(onSwipe)
 
+  const isHandEnabled = userPlay === GameMoves.EMPTY || userPlay === move
+
   return (
-    <PanGestureHandler onGestureEvent={panGestureEvent}>
+    <PanGestureHandler enabled={isHandEnabled} onGestureEvent={panGestureEvent}>
       <Animated.View style={shadowStyle}>
         <Animated.Image style={[styles.container, rStyle]} {...imageProps} />
       </Animated.View>
